bd52195148da7e0e1d46b61d86f4d02970a04d71
22DP2RAlek <131154373+22DP2RAlek@users.noreply.github.com>
2025-06-19 05:56:54 +0000

    port

--- backend/server.js
+++ backend/server.js
@@ -322,7 +322,7 @@ app.delete('/api/savedspots/:id', async (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
